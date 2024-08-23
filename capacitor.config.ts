6beca5d833ed8061c7b3a105a2cb6d79ad39a70c
Daniel Alvarez <j.daniel.alvarez.cornejo@gmail.com>
2024-08-23 01:36:19 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LoaderComponent',
+  webDir: 'www'
+};
+
+export default config;
